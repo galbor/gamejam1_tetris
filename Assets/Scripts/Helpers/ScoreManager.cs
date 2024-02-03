437c106cf0a3c8de1944ff5a473f5a99cc489340
ohadklein <ohad.klein@mail.huji.ac.il>
2024-02-03 21:06:19 +0200

    Score start

---
+++ Assets/Scripts/Helpers/ScoreManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    
+    private int _score;
+    private int _highScore;
+    
+}
